9e9286acdd355059edfa6e9d626fdf598edac42e
= <=>
2024-10-21 01:06:05 +0300

    ff

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,7 @@
 
-export const BACKEND_URL = 'https://a011-93-175-7-121.ngrok-free.app/'
+export const BACKEND_URL = 'http://45.12.18.254:4001/'
+// export const BACKEND_URL = 'http://127.0.0.1:8000/'
+
 export const BACKEND_MEDIA_PORT = '8000';
 
 export const API_URL = BACKEND_URL + 'api';
